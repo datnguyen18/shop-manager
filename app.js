0f8d40e45e469e06a328a29c80b34867183a39f0
Unknown <dat.nguyentien.set13@eiu.edu.vn>
2018-10-29 17:46:56 +0700

    setup graphql

--- app.js
+++ app.js
@@ -2,21 +2,34 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const customerRoute = require('./api/routes/customer');
-const categoryRoute = require('./api/routes/category');
-const productRoute = require('./api/routes/product');
-const orderRoute = require('./api/routes/order');
-const orderItemRoute = require('./api/routes/order_item');
+const Category = require('./api/models/category');
+const {ApolloServer, gql} = require('apollo-server');
 app.use(morgan('dev'));
 
 app.use(bodyParser.urlencoded({
     extended: false
 }))
 app.use(bodyParser.json());
+const typeDefs = gql `
+  scalar Date
 
-app.use('/customer',customerRoute);
-app.use('/category', categoryRoute);
-app.use('/product', productRoute);
-app.use('/order',orderRoute);
-app.use('/order-item',orderItemRoute);
+  type Category {
+    id: Int
+    name:String
+    description:String
+    image:String
+    createdAt:Date
+    updatedAt:Date
+  }
+  type Query {
+    categories: [Category]
+  }
+`
+
+const resolvers = {
+  Query: {
+    categories: () => categories.Category.findAll()
+  }
+}
+
 module.exports = app;
--- server.js
+++ server.js
@@ -1,9 +1,53 @@
+const Product = require('./api/models/product');
+const Category = require('./api/models/category');
+const {ApolloServer, gql} = require('apollo-server');
 
-const http = require('http');
-const port = process.env.PORT || 3000;
-const app = require('./app');
-const server = http.createServer(app);
+const typeDefs = gql `
+  scalar Date
 
-server.listen(port, ()=> {
-    console.log(`Server start on ${port}`);
+  type Product {
+    id: Int
+    catId: Int
+    name: String
+    description: String
+    image: String
+    price: Int
+    createdAt:Date
+    updatedAt:Date
+  }
+
+  type Category{
+    id: Int
+    name: String
+    description: String
+    image: String
+    createdAt: Date
+    Products: [Product]
+    updatedAt: Date
+  }
+  
+  type Query {
+    products: [Product]
+    categories: [Category]
+  }
+
+  type Mutation {
+    createProduct(catId: Int, name: String, description: String, image: String, price: Int): Product
+  }
+`
+
+const resolvers = {
+  Query: {
+    products: () => Product.findAll(),
+    categories: () => Category.findAll({
+      include: [Product]
+    })
+
+  }
+
+}
+
+const server = new ApolloServer({typeDefs, resolvers});
+server.listen().then(({url}) => {
+  console.log(`server ready at ${url}`)
 })
