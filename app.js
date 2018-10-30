2eabd8122e4559b06ab12d6e5f06862fdf865256
datnguyen <dat.nguyentien.set13@eiu.edu.vn>
2018-10-30 10:59:25 +0700

    fix server.js and app.js

--- app.js
+++ app.js
@@ -2,34 +2,21 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const Category = require('./api/models/category');
-const {ApolloServer, gql} = require('apollo-server');
+const customerRoute = require('.api/routes/customer');
+const categoryRoute = require('.api/routes/category');
+const productRoute = require('.api/routes/product');
+const orderRoute = require('.api/routes/order');
+const orderItemRoute = require('.api/routes/order_item');
 app.use(morgan('dev'));
 
 app.use(bodyParser.urlencoded({
     extended: false
 }))
 app.use(bodyParser.json());
-const typeDefs = gql `
-  scalar Date
 
-  type Category {
-    id: Int
-    name:String
-    description:String
-    image:String
-    createdAt:Date
-    updatedAt:Date
-  }
-  type Query {
-    categories: [Category]
-  }
-`
-
-const resolvers = {
-  Query: {
-    categories: () => categories.Category.findAll()
-  }
-}
-
+app.use('/customer',customerRoute);
+app.use('/category', categoryRoute);
+app.use('/product', productRoute);
+app.use('/order',orderRoute);
+app.use('/order-item',orderItemRoute);
 module.exports = app;
--- server.js
+++ server.js
@@ -1,51 +1,7 @@
-const Product = require('./api/models/product');
-const Category = require('./api/models/category');
 const {ApolloServer, gql} = require('apollo-server');
-
-const typeDefs = gql `
-  scalar Date
-
-  type Product {
-    id: Int
-    catId: Int
-    name: String
-    description: String
-    image: String
-    price: Int
-    createdAt:Date
-    updatedAt:Date
-  }
-
-  type Category{
-    id: Int
-    name: String
-    description: String
-    image: String
-    createdAt: Date
-    Products: [Product]
-    updatedAt: Date
-  }
-  
-  type Query {
-    products: [Product]
-    categories: [Category]
-  }
-
-  type Mutation {
-    createProduct(catId: Int, name: String, description: String, image: String, price: Int): Product
-  }
-`
-
-const resolvers = {
-  Query: {
-    products: () => Product.findAll(),
-    categories: () => Category.findAll({
-      include: [Product]
-    })
-
-  }
-
-}
+const schema = require('./schema');
+const resolvers= require('./resolvers')
+const typeDefs =schema
 
 const server = new ApolloServer({typeDefs, resolvers});
 server.listen().then(({url}) => {
