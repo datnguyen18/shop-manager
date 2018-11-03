864c0edf9607686fc61b165ae17d3630789170d8
datnguyen <dat.nguyentien.set13@eiu.edu.vn>
2018-11-03 19:31:28 +0700

    add schema index

---
+++ api/schema/schema.js
@@ -0,0 +1,19 @@
+const Category = require('./category');
+const Customer = require('./customer');
+const Login = require('./login');
+const Mutation = require('./mutation');
+const OrderItem = require('./order-item');
+const Order = require('./order');
+const Product = require('./product');
+const Query = require('./query');
+
+module.exports = [
+  Category,
+  Customer,
+  Login,
+  Mutation,
+  OrderItem,
+  Order,
+  Product,
+  Query,
+];
