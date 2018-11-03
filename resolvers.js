a4355759f4f5eb52b1eb9e6aa10b39c8fd0f6d6f
datnguyen <dat.nguyentien.set13@eiu.edu.vn>
2018-11-03 19:30:40 +0700

    add resolver

--- resolvers.js
+++ resolvers.js
@@ -1,34 +1,6 @@
-const Product = require('./api/models/product');
-const Category = require('./api/models/category');
-const Customer = require('./api/models/customer');
-const promisify = require('./api/helpers/');
+const Query = require('./api/resolvers/query');
+const Mutation = require('./api/resolvers/mutation');
+const Category = require('./api/resolvers/category');
 
-const resolvers = {
-  Query: {
-    categories: () => Category.findAll({
-      include: [Product],
-    }),
-  },
-
-  Mutation: {
-    addProduct: (parent, args) => Product.create(args),
-    updateProduct: (parent, { id, ...args }) => Product
-      .find({
-        where: { id },
-      })
-      .then(product => product.update(args)),
-    deleteProduct: (parent, { id }) => Product
-      .find({
-        where: { id },
-      })
-      .then(product => product.destroy()),
-    addCategory: (parent, args) => Category.create(args),
-    addCustomer: (parent, args) => Customer.create(args.input),
-    updateCustomer: (parent, { id, ...args }) => promisify(Customer.findById(id))
-      .then(customer => customer.update(args)),
-    deleteCustomer: (parent, { id }) => promisify(Customer.findById(id))
-      .then(customer => customer.destroy()),
-  },
-
-};
+const resolvers = { Query, Mutation, Category };
 module.exports = resolvers;
