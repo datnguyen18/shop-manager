a801aa2d7180ef709bd219dc6b57c5df155d28c6
datnguyen <dat.nguyentien.set13@eiu.edu.vn>
2018-11-03 19:29:59 +0700

    add relation for login model

--- api/models/login.js
+++ api/models/login.js
@@ -1,9 +1,12 @@
 const Sequelize = require('sequelize');
+const Customer = require('./customer');
 const { sequelize } = require('./config');
 
 const Login = sequelize.define('Login', {
   username: Sequelize.STRING,
   password: Sequelize.STRING,
 }, {});
+
+Login.belongsTo(Customer);
 
 module.exports = Login;
